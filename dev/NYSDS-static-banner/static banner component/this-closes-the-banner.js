9abc58fcc756ce12ebc41dca7cbc59c9a48eed66
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2022-02-14 13:45:41 -0500

    separate out component pieces
    add some readme's as well

---
+++ dev/NYSDS-static-banner/static banner component/this-closes-the-banner.js
@@ -0,0 +1,7 @@
+var closeclose = document.getElementById("nysds-close");
+if(closeclose){
+  closeclose.addEventListener("click", closestatic);
+  function closestatic () {
+  document.getElementById("nysds-hide-this").style.display = "none";
+  }
+  }
